63fe525c6ab926d002722b6212dcb12a800d863a
igormilla <igor.fsp.milla@gmail.com>
2018-05-25 17:00:53 +0200

    Making everything dynamic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import json
+import sys
 from dateutil import rrule
 from datetime import datetime
 from dateutil.parser import parse
+
+
+def year_to_timestamp(year):
+    date = parse(str(year) + "-01-01T00:00:00-00:00")
+    return int(date.strftime('%s')) * 1000
 
 
 def hour_per_day(in_locations):
@@ -19,8 +25,8 @@ def hour_per_day(in_locations):
     list_len = len(day_locations)
     time_at_location = []
     for i in range(0, list_len):
-        lat = abs(day_locations[i]['latitude'] - 413851990)
-        lon = abs(day_locations[i]['longitude'] - 21429410)
+        lat = abs(day_locations[i]['latitude'] - locations_data['office']['latitude'])
+        lon = abs(day_locations[i]['longitude'] - locations_data['office']['longitude'])
         if lat + lon < 50000:
             time_at_location.append({
                 'timestamp': day_locations[i]['timestamp'],
@@ -37,16 +43,27 @@ def get_readable_date_from_ms(timestamp_ms):
     return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 
+# get YEAR from command line
+if len(sys.argv) > 1:
+    YEAR = sys.argv[1:][0]
+else:
+    raise Exception('You kinda need to pass a YEAR to get data for a year')
+
+
+# read locations data
+with open('locations.json') as file:
+    locations_data = json.load(file)
+
 # read data
-j_file = './data/1483225200000.json'
+j_file = './data/' + str(year_to_timestamp(YEAR)) + '.json'
 with open(j_file) as train_file:
     dict_train = json.load(train_file)
 
 locations = dict_train
 
 # get weekly breakdown
-now = parse("2017-01-01T00:00:00-00:00")
-then = parse("2018-01-01T00:00:00-00:00")
+now = parse(YEAR + "-01-01T00:00:00-00:00")
+then = parse(str((int(YEAR) + 1)) + "-01-01T00:00:00-00:00")
 weeks = []
 for dt in rrule.rrule(rrule.WEEKLY, dtstart=now, until=then):
     weeks.append(int(dt.strftime('%s')))
